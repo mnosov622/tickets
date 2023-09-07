b4fcd32701c676dc1e0489a0d02c48d08672bbc7
Maxim N <mnosov622@gmail.com>
2023-09-07 07:16:00 -0400

    styling update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import Plane from "./assets/plane.png";
+import Filters from "./components/Filters/Filters";
 import TicketCard from "./components/TicketCard/TicketCard";
-
+import "./App.css";
 const App = () => {
   return (
     <section>
       <section className="img-container">
         <img src={Plane} alt="plane" />
       </section>
-
-      <section className="filters"></section>
-
-      <section>
+      <main className="main">
+        <Filters />
         <TicketCard />
-      </section>
+      </main>
     </section>
   );
 };
--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,7 +1,41 @@
+import React, { useState } from "react";
 import "./Filters.css";
 
 const Filters = () => {
-  return <section>Filters</section>;
+  const [selectedCurrency, setSelectedCurrency] = useState<string>("RUB");
+
+  const handleCurrencyClick = (currency: string) => {
+    setSelectedCurrency(currency);
+  };
+
+  return (
+    <section className="filters">
+      <div className="currency">
+        <h3>Валюта</h3>
+        <div className="currency-buttons">
+          <button
+            className={`currency-button ${selectedCurrency === "RUB" ? "blue" : ""}`}
+            onClick={() => handleCurrencyClick("RUB")}
+          >
+            RUB
+          </button>
+          <button
+            className={`currency-button ${selectedCurrency === "USD" ? "blue" : ""}`}
+            onClick={() => handleCurrencyClick("USD")}
+          >
+            USD
+          </button>
+          <button
+            className={`currency-button ${selectedCurrency === "EUR" ? "blue" : ""}`}
+            onClick={() => handleCurrencyClick("EUR")}
+          >
+            EUR
+          </button>
+          <div />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Filters;
